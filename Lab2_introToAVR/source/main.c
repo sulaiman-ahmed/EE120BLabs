aaa755a15650086bb18cf393542ada0224ab0913
Sulaiman Ahmed <sulaimanahmed08@gmail.com>
2021-04-08 08:08:11 -0700

    Worked on setup

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -14,10 +14,21 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF;
+    DDRB = 0xFF;
+    PORTB = 0x00;
 
-    /* Insert your solution below */
-    while (1) {
+    unsigned char tmpB = 0x00;
+    unsigned char tmpA = 0x00;
 
+    while(1){
+	    tmpA = PINA & 0x01;
+	    if(tmpA == 0x01) {
+		    tmpB = (tmpB & 0xFC) | 0x01;
+	    } else {
+		    tmpB = (tmpB & 0xFC) | 0x02;
+	    }
+	    PORTB = tmpB;
     }
-    return 1;
+    return 0;
 }
